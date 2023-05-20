0f2e9dfe70a7ffb25fb343f02343d955df694f79
linden hutchinson <41136671+lindenhutchinson@users.noreply.github.com>
2023-05-20 13:11:24 +1000

    register models with admin site

--- MemeComp/app/admin.py
+++ MemeComp/app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Competition, Vote, Participant, Meme, SeenMeme
 
+admin.site.register(User)
+admin.site.register(Competition)
+admin.site.register(Vote)
+admin.site.register(Participant)
+admin.site.register(Meme)
+admin.site.register(SeenMeme)
